b2d9344186b0871c7e0f6d2c3f8bdfa02c0528a3
KodeHijau <suratsofyan@gmail.com>
2013-06-27 17:18:01 +0700

    PERTAMAX GANN 0(@@)0 cendolnya gan

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jun 27, 2013
+ *      Author: kodehijau
+ */
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	cout << "HELLO KANDANG 0(@@)0";
+
+	return 0;
+}
+
